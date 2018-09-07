f30c7afef2d7505c36da44ebfc3349f0b99e8f7d
Evan Burrell <burrelle@oregonstate.edu>
2018-09-07 15:01:12 -0700

    Format blade

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
-        <title>Evan Burrell</title>
-    </head>
-    <body class="antialiased font-sans bg-grey-lightest">
-        @yield('body')
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
+    <title>Evan Burrell</title>
+</head>
+
+<body class="antialiased font-sans bg-grey-lightest">
+    @yield('body')
+</body>
+
 </html>
