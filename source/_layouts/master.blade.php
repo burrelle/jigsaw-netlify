68435ecb15ff3bcc795e74dd67e17435490fd2bd
Evan Burrell <burrelle@oregonstate.edu>
2018-09-17 08:53:15 -0700

    Syntax highlighting

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/arduino-light.min.css">
     <title>Evan Burrell</title>
 </head>
 
 <body class="antialiased font-sans bg-grey-lightest">
     @yield('body')
 </body>
-
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 </html>
